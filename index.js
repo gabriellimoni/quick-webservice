c9cf1a91e38fe3a44ad5230380be30aafc0c439f
Gabriel Limoni <gabriel.limoni@liven.tech>
2021-06-10 21:14:55 -0300

    feat: get method is working fine!

---
+++ index.js
@@ -0,0 +1,120 @@
+const http = require("http");
+
+module.exports = {
+  build() {
+    const routes = [];
+
+    const handleRequest = async (req, res) => {
+      const url = req.url;
+      const method = req.method.toLowerCase();
+      const currentRoute = resolveRoute(url);
+
+      if (!currentRoute) {
+        res.write("Not found path ");
+        res.write(url);
+        return res.end();
+      }
+      if (method !== currentRoute.method) {
+        res.write(`Cannot ${method.toUpperCase()} `);
+        res.write(url);
+        return res.end();
+      }
+
+      req.params = currentRoute.params;
+
+      await handleRequestChain(req, res, currentRoute.chain);
+    };
+
+    const resolveRoute = (url) => {
+      const simpleRoute = routes.find(
+        (route) => route.path.toLowerCase() === url.toLowerCase()
+      );
+      if (simpleRoute) return simpleRoute;
+
+      if (url.charAt(url.length - 1) === "/") {
+        url = url.slice(0, url.length - 1);
+      }
+
+      const whichIndexHasParamsSeekUrl = [];
+      const seekUrlpPathBlocks = url.split("/");
+      for (let i = 0; i < seekUrlpPathBlocks.length; i++) {
+        const thisPathBlock = seekUrlpPathBlocks[i];
+        if (thisPathBlock.charAt(0) === ":") {
+          whichIndexHasParamsSeekUrl.push(i);
+        }
+      }
+      for (const route of routes) {
+        let match = true;
+        const pathBlocks = route.path.split("/");
+
+        if (pathBlocks.length !== seekUrlpPathBlocks.length) continue;
+
+        const whichIndexHasParams = [];
+        for (let i = 0; i < pathBlocks.length; i++) {
+          const thisPathBlock = pathBlocks[i];
+          if (thisPathBlock.charAt(0) === ":") {
+            whichIndexHasParams.push(i);
+          }
+        }
+        for (let i = 0; i < whichIndexHasParamsSeekUrl.length; i++) {
+          if (whichIndexHasParamsSeekUrl[i] !== whichIndexHasParams[i]) {
+            match = false;
+          }
+        }
+        if (match) {
+          const params = {};
+          whichIndexHasParams.forEach((indexWithParam) => {
+            params[pathBlocks[indexWithParam].slice(1)] =
+              seekUrlpPathBlocks[indexWithParam];
+          });
+          return {
+            ...route,
+            params,
+          };
+        }
+      }
+      return undefined;
+    };
+
+    const handleRequestChain = async (req, res, funcs) => {
+      let currentResponse = null;
+      for (let i = 0; i < funcs.length; i++) {
+        const currentFunc = funcs[i];
+        currentResponse = await currentFunc(req, res);
+        if (res.finished) break;
+      }
+      if (!currentResponse) {
+        res.writeHead(200);
+        return res.end();
+      }
+      res.writeHead(currentResponse.status ? currentResponse.status : 200);
+      res.write(
+        currentResponse.data
+          ? currentResponse.data
+          : currentResponse
+          ? currentResponse
+          : ""
+      );
+      res.end();
+    };
+
+    const server = http.createServer(handleRequest);
+
+    return {
+      get(path, ...funcs) {
+        routes.push({
+          path: path,
+          method: "get",
+          chain: funcs,
+        });
+      },
+      listen(port, callback) {
+        server.listen(port, callback);
+      },
+      close() {
+        server.close();
+      },
+      server,
+    };
+  },
+};
---
+++ index.spec.js
@@ -0,0 +1,96 @@
+const QuickWebservice = require("./index");
+const request = require("supertest");
+
+let quickWebservice = QuickWebservice.build();
+
+beforeEach(() => {
+  quickWebservice = QuickWebservice.build();
+});
+afterEach(() => {
+  quickWebservice.close();
+});
+
+describe("GET routes", () => {
+  test("Should return 200 and string data on text response", async () => {
+    quickWebservice.get("/", () => {
+      return "body-response";
+    });
+    quickWebservice.listen(3000);
+
+    const response = await request(quickWebservice.server).get("/");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("body-response");
+  });
+
+  test("Should return 303 and data on text response", async () => {
+    quickWebservice.get("/303", () => {
+      return { status: 303, data: "body-response" };
+    });
+    quickWebservice.listen(3000);
+
+    const response = await request(quickWebservice.server).get("/303");
+    expect(response.status).toBe(303);
+    expect(response.text).toBe("body-response");
+  });
+
+  test("Should return 200 and no data if no response is provided", async () => {
+    quickWebservice.get("/", () => {
+      return;
+    });
+    quickWebservice.listen(3000);
+
+    const response = await request(quickWebservice.server).get("/");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("");
+  });
+
+  test("Should parse one route param correctly", async () => {
+    quickWebservice.get("/param/:myParam", ({ params }) => {
+      return params.myParam;
+    });
+    quickWebservice.listen(3000);
+
+    const response = await request(quickWebservice.server).get(
+      "/param/paramToTest"
+    );
+    expect(response.text).toBe("paramToTest");
+  });
+
+  test("Should parse many route param correctly", async () => {
+    quickWebservice.get(
+      "/param/:myParam1/:myParam2/another-path/:myParam3",
+      ({ params }) => {
+        return `${params.myParam1}-${params.myParam2}-${params.myParam3}`;
+      }
+    );
+    quickWebservice.listen(3000);
+
+    const response = await request(quickWebservice.server).get(
+      "/param/firstParam/secondParam/another-path/thirdParam"
+    );
+    expect(response.text).toBe("firstParam-secondParam-thirdParam");
+  });
+});
+
+/*
+const QuickWebservice = require("./index");
+const quickWebservice = QuickWebservice.build();
+
+quickWebservice.get("/", () => {
+  return "FOI";
+});
+quickWebservice.get("/teste", () => {
+  return { status: 200, data: "FOI TESTE" };
+});
+quickWebservice.get("/teste/:id", () => {
+  return { status: 200, data: "FOI TESTE ID" };
+});
+quickWebservice.get("/teste/:id/:teste/gabriel/:carai", ({ params }) => {
+  console.log(params);
+  return { status: 200, data: "FOI TESTE TESTEEEE" };
+});
+
+quickWebservice.listen(3000, function () {
+  console.log("Listening on 3000");
+});
+*/
